0ff4c2e8c06fc833d354a22856698ebc6f9c297b
fanqiang <oge@live.com>
2024-08-04 16:48:35 +0800

    添加testcontainers代码和相关配置

--- src/main/resources/archetype-resources/src/test/java/controller/HelloControllerTest.java
+++ src/main/resources/archetype-resources/src/test/java/controller/HelloControllerTest.java
@@ -4,12 +4,28 @@
 package ${package}.controller;
 
 
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.api.model.Ports;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.function.Supplier;
 
 
 @SpringBootTest
@@ -18,9 +34,55 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @AutoConfigureMockMvc
 class HelloControllerTest {
 
+    static GenericContainer<?> redis = new GenericContainer<>(
+            DockerImageName.parse("redis:latest")
+    )
+            .withExposedPorts(6379);
+
+    static MySQLContainer<?> mysql = new MySQLContainer<>(
+            DockerImageName.parse("dockerhub.qingcloud.com/doubao/mysql:9.0.1").asCompatibleSubstituteFor("mysql")
+    ).withAccessToHost(true)
+            .withUsername("root")
+            .withExposedPorts(3306)
+            .waitingFor(Wait.forListeningPort())
+            .withCreateContainerCmdModifier(cmd -> cmd.withHostConfig(
+                    new HostConfig().withPortBindings(
+                            new PortBinding(Ports.Binding.bindPort(33064), new ExposedPort(3306))
+                    )
+            ));
+
+
+
+
+    @DynamicPropertySource
+    static void redisProperties(DynamicPropertyRegistry registry) throws SQLException {
+
+        redis.start();
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", redis::getFirstMappedPort);
+
+        mysql.start();
+        Connection connection = DriverManager.getConnection(mysql.getJdbcUrl(), mysql.getUsername(), mysql.getPassword());
+        Statement statement = connection.createStatement() ;
+        String createPrimaryDatabaseSQL = "CREATE DATABASE doubao_primary_test";
+        String createSlaveDatabaseSQL = "CREATE DATABASE doubao_slave_test";
+
+        statement.executeUpdate(createPrimaryDatabaseSQL);
+        statement.executeUpdate(createSlaveDatabaseSQL);
+
+        Supplier<String> primary = () -> mysql.getJdbcUrl().replace("test", "doubao_primary_test");
+        Supplier<String> slave = () -> mysql.getJdbcUrl().replace("test", "doubao_slave_test");
+        registry.add("spring.datasource.dynamic.datasource.primary.url", primary::get);
+        registry.add("spring.datasource.dynamic.datasource.primary.username", mysql::getUsername);
+        registry.add("spring.datasource.dynamic.datasource.primary.password", mysql::getPassword);
+        registry.add("spring.datasource.dynamic.datasource.slave.url", slave::get);
+        registry.add("spring.datasource.dynamic.datasource.slave.username", mysql::getUsername);
+        registry.add("spring.datasource.dynamic.datasource.slave.password", mysql::getPassword);
+    }
 
     @Test
     void hello(){
         System.out.println("hello");
     }
 }
+
